0b850ea1d65bae90b11aba0596cf39e89eb8ac54
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-11-21 11:40:08 +0500

    added inline html server

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ http.createServer((req, res) => {
     res.end("Response from the server");
   })
   .listen(8282, "127.0.0.1");
+
+//if we want to display html
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write(
+    `
+    <html>
+    <body>
+    <h1 style="background:black">INSIDE THE SERVER!</h1>
+    </body>
+    </html>
+    `)
+    res.end("Response from the server");
+  })
+  .listen(8282, "127.0.0.1");
+
+ 
