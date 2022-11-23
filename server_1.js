7c0278741387c538a9480b84259a55ff2f66b785
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-11-23 12:48:33 +0500

    added new response

--- server_1.js
+++ server_1.js
@@ -5,11 +5,19 @@ const app = express()
 app.get('/', (req, res) => {
     res.send("<html><body><h1>Hello there</h1></body></html>")
 })
-app.get("/api/data", (req, res) => {
+app.get("/api/data/name", (req, res) => {
     res.send({
         name: "hello",
         age: "20"
     });
+})
+app.get('/api/data/cars',(req,res)=>{
+res.send({
+    company:"Honda",
+    Model:"Civic",
+    company:"Honda",
+    Model:"City"
+})
 })
 const port = process.env.port || 3000
 app.listen(port)
