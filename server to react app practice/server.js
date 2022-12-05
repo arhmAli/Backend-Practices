ffd773f26b84b4d98ec76b003989084274cc5139
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-05 08:10:57 +0500

    successfully connected to db

--- server to react app practice/server.js
+++ server to react app practice/server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require('mongodb');
-const mongoUrl = "mongodb+srv://adminname:somepassword@cluster0.xslqt8v.mongodb.net/?retryWrites=true&w=majority"
-app.get('/api/users', (req, res) => {
-    res.json([
-        {
-            id: 1,
-            username: "francis"
-        },
-        {
-            id: 2,
-            username: "steve"
-        }
-    ])
+const mongoUrl = "mongodb+srv://admin:randompassword@cluster0.xslqt8v.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(mongoUrl);
+app.get('/api/users', async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db('myApp');
+        const collection = database.collection("users");
+        const query = await collection.insertOne({
+            name: "arham",
+            lastname: "khan"
+        });
+        res.status(200).json({ responding: "passed" })
+    }
+    catch (err) {
+        throw err
+    }
+    finally {
+        await client.close();
+        console.log("The whole process has been finished")
+
+    }
 })
 app.get('/', (req, res) => {
     res.send(
