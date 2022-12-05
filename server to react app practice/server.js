77744594115cf1ea2c7580fbedf4192777cb04cc
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-05 07:51:39 +0500

    connected mongodb

--- server to react app practice/server.js
+++ server to react app practice/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const { MongoClient } = require('mongodb');
+const mongoUrl = "mongodb+srv://adminname:somepassword@cluster0.xslqt8v.mongodb.net/?retryWrites=true&w=majority"
 app.get('/api/users', (req, res) => {
     res.json([
         {
@@ -19,6 +20,14 @@ app.get('/', (req, res) => {
             name: "hello",
             age: "20"
         })
+})
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) {
+        throw err
+    }
+    else {
+        console.log("connected to the mongodb")
+    }
 })
 const port = process.env.port || 5000;
 app.listen(port)
