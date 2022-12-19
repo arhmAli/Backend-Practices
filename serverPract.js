2b12e433ab658c0ec6f9a41086ae70f4dbd6e32d
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-19 08:58:11 +0500

    created serverPract

---
+++ serverPract.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+const fs=require("fs");
+
+const bodyParser=require("body-parser");
+
+app.get("/",(req,res)=>{
+  console.log(req)
+  )
+  
+  app.get("/",(req,res)=>{
+  console.log(res)
+  })
