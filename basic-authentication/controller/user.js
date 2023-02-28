d77b2e41fe93ce6e05f0c4e21d0f85140c86a044
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-03-01 00:34:45 +0500

    tried making controller

---
+++ basic-authentication/controller/user.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import User from '../model/User.js'
+import bcrypt from 'bcrypt'
+
+export default cont=async(req,res)=>{
+try{
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        const newUser=new User({...req.body,password:hash});
+        await newUser
+}
+}
