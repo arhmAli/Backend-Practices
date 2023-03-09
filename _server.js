aa04ac0276f46dbb71fde8c84033d43a90152b74
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-03-09 08:41:14 +0500

    db connected in _server

--- _server.js
+++ _server.js
@@ -2,8 +2,12 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 const app=express();
-
+const connect=mongoose.connect((process.env.port).then(
+console.log("Connected to the db").catch(e)=>(console.log(e)
+)
 const port =process.env.port||8800
 
-app.listen((port)=>
-          console.error(`PORT RUNNING ON ${port}`))
+app.listen((port)=>{
+          connect();
+          console.error(`PORT RUNNING ON ${port}`)
+})
